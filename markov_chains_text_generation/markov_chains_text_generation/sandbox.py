4da7c901d536eed832628cc9913d912ad7156939
Wojtek Majewski <jumski@gmail.com>
2020-05-07 17:54:41 +0200

    Super simple counting of occurences

--- markov_chains_text_generation/markov_chains_text_generation/sandbox.py
+++ markov_chains_text_generation/markov_chains_text_generation/sandbox.py
@@ -8,20 +8,33 @@ def read_dialogs(csv_path):
 
         return [row[3] for row in reader][1:]
 
+class MarkovChain:
+    def __init__(self):
+        self._rows = defaultdict(self._create_row)
+
+    def add_occurence(self, from_val, to_val):
+        self._rows[from_val][to_val] +=1
+
+    def items(self):
+        return self._rows.items();
+
+    def _create_row(_):
+        return defaultdict(lambda: 0)
+
 def main():
     csv_path = "data/clean_dialog.csv"
     dialogs = read_dialogs(csv_path)
     dialog = dialogs[0]
 
-    def create_row():
-        return defaultdict(lambda: 0)
-    chain = defaultdict(create_row)
+    chain = MarkovChain()
 
     words = dialog.split()
     for prev,curr in itertools.zip_longest(words, words[1:]):
-        chain[prev][curr] += 1
+        chain.add_occurence(prev, curr)
 
-    print(chain)
-
-
-    # print(dialogs[0])
+    for from_word,to_words in chain.items():
+        print('FROM WORD: ', from_word)
+        for to_word,count in to_words.items():
+            print("  ", to_word, " = ", count)
+        # for to_word,count in to_words:
+        #     print('.')
